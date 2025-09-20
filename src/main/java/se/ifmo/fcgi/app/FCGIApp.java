c41be5d459aabbca04677bc2aee44ac4ffb89867
nafine <pavel.nafine@gmail.com>
2025-09-20 13:03:22 +0300

    request writing fix

--- src/main/java/se/ifmo/fcgi/app/FCGIApp.java
+++ src/main/java/se/ifmo/fcgi/app/FCGIApp.java
@@ -3,6 +3,9 @@ package se.ifmo.fcgi.app;
 import com.fastcgi.FCGIInterface;
 import se.ifmo.fcgi.http.HTTPAdapter;
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
@@ -24,23 +27,35 @@ public abstract class FCGIApp<Request, Response> implements Runnable, HTTPAdapte
     private void handle() {
         try {
             if (!allowedMethods.contains(FCGIInterface.request.params.getProperty("REQUEST_METHOD"))) {
-                System.out.println("Method not allowed.");
+                System.out.println("""
+                        HTTP/1.1 405 Method Not Allowed\r
+                        Content-Length: 0\r
+                        Allow: GET\r
+                        """);
+                return;
             }
 
             Request rq = toRequest(FCGIInterface.request.params.getProperty("QUERY_STRING"));
             Response rs = process(rq);
             String rsStr = toHTTPResponse(rs);
             String headers = """
-                    HTTP/1.1 200 OK
-                    Content-Type: application/json
-                    Content-Length: %d
-                    
-                    %s
-                    
-                    """;
+                    HTTP/1.1 200 OK\r
+                    Content-Type: application/json\r
+                    Content-Length: %d\r
+                    \r
+                    %s""";
             System.out.printf(headers, rsStr.getBytes(StandardCharsets.UTF_8).length, rsStr);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            String headers = """
+                    HTTP/1.1 400 Bad Request\r
+                    Content-Type: application/json\r
+                    Content-Length: %d\r
+                    \r
+                    %s""";
+            String json = """
+                    {"error": "Bad Request","message": "%s"}
+                    """.formatted(e.getMessage());
+            System.out.printf(headers, json.getBytes(StandardCharsets.UTF_8).length, json);
         }
     }
 
